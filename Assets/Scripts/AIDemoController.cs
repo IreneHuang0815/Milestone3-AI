cbc762016387eb77bab9536634200f71aea8d424
IreneHuang0815 <yhuang410@gatech.edu>
2017-10-17 00:00:22 -0400

    Commit with chasing implemented

--- Assets/Scripts/AIDemoController.cs
+++ Assets/Scripts/AIDemoController.cs
@@ -8,31 +8,26 @@ using UnityEngine.AI;
 //Added:
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
 public class AIDemoController : MonoBehaviour
 {
 
 
     public Transform[] waypointSetA;
 
-    public Transform[] waypointSetB;
-
-    public Transform[] waypointSetC;
-
     public Transform waypointE;
 
-    public Vector3 waypointF;
+    public Vector3 movingDirection;
+	public Vector3 throwingDirection;
 
     public enum State
     {
 
         A,
-        B,
-        C,
         D,
         E,
         F,
-		G,
-		H
+		G
     }
 
 
@@ -47,7 +42,9 @@ public class AIDemoController : MonoBehaviour
     AINavSteeringController aiSteer;
     NavMeshAgent agent;
 	//Added:
+	bool targetHit = false;
 	Animator anim;
+	GameObject player;
 	GameObject target;
 	GameObject[] possibleTargets;
 
@@ -56,10 +53,9 @@ public class AIDemoController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+		player = GameObject.FindGameObjectWithTag ("player");
 		possibleTargets = GameObject.FindGameObjectsWithTag ("target");
         aiSteer = GetComponent<AINavSteeringController>();
-
         agent = GetComponent<NavMeshAgent>();
 		//Added
 		anim = GetComponent<Animator>();
@@ -101,8 +97,9 @@ public class AIDemoController : MonoBehaviour
 	}
 
 	void OnCollisionEnter(Collision theCollision){
-		if (theCollision.gameObject.tag == "target"){
-		Debug.Log ("Hit something!");
+		if (theCollision.gameObject == target){
+			Debug.Log ("target hit!!!!");
+			targetHit = true;
 		}
 	}
 
@@ -121,31 +118,7 @@ public class AIDemoController : MonoBehaviour
     }
 
 
-    void transitionToStateB()
-    {
-
-        print("Transition to state B");
-
-        state = State.B;
-
-        aiSteer.setWaypoints(waypointSetB);
-
-        aiSteer.useNavMeshPathPlanning = true;
-    }
-
-
-    void transitionToStateC()
-    {
-
-        print("Transition to state C");
-
-        state = State.C;
-
-        aiSteer.setWaypoints(waypointSetC);
-
-        aiSteer.useNavMeshPathPlanning = false;
-
-    }
+//
 
     void transitionToStateD()
     {
@@ -176,90 +149,117 @@ public class AIDemoController : MonoBehaviour
 
     }
 
-    void transitionToStateF()
+    void transitionToStateF(GameObject target)
     {
-
-        print("Transition to state F");
-
+        print("Transition to state F : Chasing enemy");
         state = State.F;
-
-        aiSteer.setWaypoint(waypointF);
-
-        aiSteer.useNavMeshPathPlanning = true;
-
+		Vector3 shootPos = player.transform.position;
+		Vector3 targetPos = target.transform.position;
+		Vector3 shooterVel = player.GetComponent<Rigidbody>() ? player.GetComponent<Rigidbody>().velocity : Vector3.zero;
+		Vector3 targetVel = target.GetComponent<Rigidbody>() ? target.GetComponent<Rigidbody>().velocity : Vector3.zero;
+		movingDirection = firstOrderIntercept (shootPos, shooterVel, shooterVel.magnitude, targetPos, targetVel);
+		aiSteer.setWaypoint(movingDirection);
+		aiSteer.useNavMeshPathPlanning = true;
     }
 
 	//Add a Throwing State:
-	void transitionToStateG()
+	void transitionToStateG(GameObject target)
 	{
 		print ("Transition to staste G: Throwing");
 		state = State.G;
 		anim.SetTrigger("Throw");
 	}
+		
 
-	//Add a chasing State
-	void transitionToStateH()
+	Vector3 firstOrderIntercept(Vector3 shooterPosition,
+		Vector3 shooterVelocity,
+		float shotSpeed,
+		Vector3 targetPosition,
+		Vector3 targetVelocity)
 	{
-		print ("Transition to State H : chasing enemy");
-		state = State.H;
-		target = findClosestEnemy();
-		aiSteer.setWaypoint (target.transform);
-		aiSteer.useNavMeshPathPlanning = true;
+		Vector3 targetRelativePosition = targetPosition - shooterPosition;
+		Vector3 targetRelativeVelocity = targetVelocity - shooterVelocity;
+		float t = firstOrderInterceptTime(shotSpeed, targetRelativePosition, targetRelativeVelocity);
+		return targetPosition + t * (targetRelativeVelocity);
+	}
+
+	float firstOrderInterceptTime (float shotSpeed, Vector3 targetRelativePosition, Vector3 targetRelativeVelocity)
+	{
+		float velocitySquared = targetRelativeVelocity.sqrMagnitude;
+		if (velocitySquared < 0.001f) {
+			return 0f;
+		}
+		float a = velocitySquared - shotSpeed * shotSpeed;
+		if (Mathf.Abs (a) < 0.001f) {
+			float t = -targetRelativePosition.sqrMagnitude / (2f * Vector3.Dot
+				(
+				          targetRelativeVelocity,
+				          targetRelativePosition
+			          ));
+			return Mathf.Max (t, 0f);
+		}
+		float b = 2f * Vector3.Dot (targetRelativeVelocity, targetRelativePosition);
+		float c = targetRelativePosition.sqrMagnitude;
+		float determinant = b * b - 4f * a * c;
+		if (determinant > 0f) {
+			float t1 = (-b + Mathf.Sqrt (determinant)) / (2f * a);
+			float t2 = (-b - Mathf.Sqrt (determinant)) / (2f * a);
+			if (t1 > 0f) {
+				if (t2 > 0f) {
+					return Mathf.Min (t1, t2);
+				} else {
+					return t1;
+				}
+			} else {
+				return Mathf.Max (t2, 0f);
+			}
+		} else if (determinant < 0f) {
+			return 0f;
+		} else {
+			return Mathf.Max (-b / (2f * a), 0f);
+		}
 	}
 
     // Update is called once per frame
     void Update()
     {
-		
         switch (state)
         {
             case State.A:
-                
                 if (aiSteer.waypointsComplete())
-                    transitionToStateG();
+                    transitionToStateD();
                 break;
-			case State.G:
-				//If target hit
-				transitionToStateH();
-				break;
-			case State.H:
-				//
-				transitionToStateD ();
-				break;
-//            case State.B:
-//                if (aiSteer.waypointsComplete())
-//                    transitionToStateC();
-//                break;
-//
-//            case State.C:
-//                if (aiSteer.waypointsComplete())
-//                    transitionToStateD();
-//                break;
-
             case State.D:
                 if (Time.timeSinceLevelLoad - beginWaitTime > waitTime)
                     transitionToStateE();
                 break;
 
-            case State.E:
-                if (aiSteer.waypointsComplete())
-                    transitionToStateF();
+			case State.E:
+				if (aiSteer.waypointsComplete()) {
+					target = findClosestEnemy();
+//					player.GetComponent<Rigidbody> ().velocity = player.GetComponent<Rigidbody> ().velocity * 2f;
+					transitionToStateF (target);
+				}
                 break;
-
-            case State.F:
-                if (aiSteer.waypointsComplete())
-                    transitionToStateA();
-                break;
-			//Throw State
-			//Todo: 
-			//Add a throwing state
-			//Add chasing state: make a gameobject a waypoint
+			case State.F:
+				Debug.Log ("targethIt:   " + targetHit);
+				if (targetHit) {
+	//							player.GetComponent<Rigidbody> ().velocity = player.GetComponent<Rigidbody> ().velocity * 0.5f;
+					target = findClosestEnemy ();
+					transitionToStateG (target);
+					targetHit = false;	
+				} else {
+				transitionToStateF (target);
+				}
+				break;
+			case State.G:
+				//If target hit
+				transitionToStateA();
+				break;
             default:
 
                 print("Weird?");
                 break;
         }
-
-		 
     }
 }
