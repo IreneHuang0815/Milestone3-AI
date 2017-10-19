f4754dba0e9f55ea7fe0e04f00a2711c69c81d14
IreneHuang0815 <yhuang410@gatech.edu>
2017-10-18 23:58:40 -0400

    FinalYeah

---
+++ Assets/Prefabs/textManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class textManager : MonoBehaviour {
+	// Use this for initialization
+	public static string state;
+
+	Text text;
+	void Start () {
+		text = GetComponent<Text> ();
+		state = "A";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Currently in State: " + state;
+	}
+}
--- Assets/Scripts/AIDemoController.cs
+++ Assets/Scripts/AIDemoController.cs
@@ -15,6 +15,7 @@ public class AIDemoController : MonoBehaviour
 
     public Transform[] waypointSetA;
 
+	
     public Transform waypointE;
 
     public Vector3 movingDirection;
@@ -47,16 +48,19 @@ public class AIDemoController : MonoBehaviour
 	GameObject player;
 	GameObject target;
 	GameObject[] possibleTargets;
-
+	GameObject leftWrist;
 	GameObject theBall;
-	float throwingForce = 50f;
+	float throwingForce = 10f;
 
 	bool throwing = false;
 
+	string[] states;
+	string currentState = "A";
 
     // Use this for initializations
     void Start()
     {
+		leftWrist = GameObject.FindGameObjectWithTag ("leftWrist");
 		theBall = GameObject.FindGameObjectWithTag ("ball");
 		player = GameObject.FindGameObjectWithTag ("player");
 		possibleTargets = GameObject.FindGameObjectsWithTag ("target");
@@ -75,7 +79,12 @@ public class AIDemoController : MonoBehaviour
         aiSteer.stopAtNextWaypoint = false;
 
         transitionToStateA();
-		
+		states = new string[5];
+		states[0] = "A";
+		states[1] = "D";
+		states[2] = "E";
+		states[3] = "F";
+		states[4] = "G";
     }
 
 	GameObject randomlyChooseEnemy()
@@ -108,9 +117,43 @@ public class AIDemoController : MonoBehaviour
 		}
 	}
 
+	void randomlyChooseAState()
+	{
+		endThrowing ();
+		int index = Random.Range(0, 4);
+		while (currentState == states [index]) {
+			index = Random.Range(0, 4);
+		}
+		currentState = states [index];
+		detectState();
+	}
+
+	void detectState()
+	{
+		if (currentState == "A") {
+		
+			transitionToStateA ();
+		}
+		if (currentState == "D") {
+		
+			transitionToStateD();
+		}
+		if (currentState == "E") {
+			transitionToStateE ();
+		}
+		if (currentState == "F")
+		{
+			target = findClosestEnemy();
+			transitionToStateF(target);
+		}
+		if (currentState == "G") {
+			target = findClosestEnemy();
+			transitionToStateG(target);}
+	}
+
     void transitionToStateA()
     {
-
+		textManager.state = "A";
         print("Transition to state A");
 
         state = State.A;
@@ -118,16 +161,21 @@ public class AIDemoController : MonoBehaviour
         aiSteer.setWaypoints(waypointSetA);
 
         aiSteer.useNavMeshPathPlanning = true;
+		theBall.transform.SetParent (leftWrist.transform);
+		Debug.Log ("Balls's grand parent: " + theBall.transform.parent.name);
+		theBall.GetComponent<Rigidbody> ().isKinematic = true;
+		theBall.GetComponent<Rigidbody> ().useGravity = false;
+		theBall.GetComponent<Collider> ().enabled = false;
+
 
 
     }
+		
 
-
-//
 
     void transitionToStateD()
     {
-
+		textManager.state = "D";
         print("Transition to state D");
 
         state = State.D;
@@ -143,7 +191,7 @@ public class AIDemoController : MonoBehaviour
 
     void transitionToStateE()
     {
-
+		textManager.state = "E";
         print("Transition to state E");
 
         state = State.E;
@@ -156,6 +204,7 @@ public class AIDemoController : MonoBehaviour
 
     void transitionToStateF(GameObject target)
     {
+		textManager.state = "F";
         print("Transition to state F : Chasing enemy");
         state = State.F;
 		Vector3 shootPos = player.transform.position;
@@ -170,6 +219,7 @@ public class AIDemoController : MonoBehaviour
 	//Add a Throwing State:
 	void transitionToStateG(GameObject target)
 	{
+		textManager.state = "G";
 		print ("Transition to staste G: Throwing");
 		throwing = true;
 		Vector3 shootPos = theBall.transform.position;
@@ -177,7 +227,6 @@ public class AIDemoController : MonoBehaviour
 		Vector3 shooterVel = Vector3.zero;
 		Vector3 targetVel = target.GetComponent<Rigidbody>() ? target.GetComponent<Rigidbody>().velocity : Vector3.zero;
 		throwingDirection = firstOrderIntercept (shootPos, shooterVel, throwingForce, targetPos, targetVel);
-		throwAball(throwingDirection.normalized);
 		state = State.G;
 		anim.SetTrigger("Throw");
 	}
@@ -234,44 +283,43 @@ public class AIDemoController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+	{
         switch (state)
         {
-            case State.A:
-                if (aiSteer.waypointsComplete())
-                    transitionToStateD();
-//					target = findClosestEnemy();
-//					transitionToStateG(target);
+		case State.A:
+//			Debug.Log("")
+			if (aiSteer.waypointsComplete ())
+					randomlyChooseAState ();
                 break;
-            case State.D:
-                if (Time.timeSinceLevelLoad - beginWaitTime > waitTime)
-                    transitionToStateE();
+		case State.D:
+			if (Time.timeSinceLevelLoad - beginWaitTime > waitTime)
+				endThrowing ();
+				randomlyChooseAState ();
                 break;
 
 			case State.E:
 				if (aiSteer.waypointsComplete()) {
-					target = findClosestEnemy();
+					
 //					player.GetComponent<Rigidbody> ().velocity = player.GetComponent<Rigidbody> ().velocity * 2f;
-					transitionToStateF (target);
-				}
+				randomlyChooseAState();		
+			}
                 break;
 		case State.F:
-			if (aiSteer.waypointsComplete())
-				target = findClosestEnemy();
-				transitionToStateG(target);
-//				if (targetHit) {
-//	//							player.GetComponent<Rigidbody> ().velocity = player.GetComponent<Rigidbody> ().velocity * 0.5f;
-//					
-//					transitionToStateG (target);
-//					targetHit = false;	
-//				} else {
-//				transitionToStateF (target);
-//				}
+			if (aiSteer.waypointsComplete ())
+				randomlyChooseAState ();
+				if (targetHit) {
+					player.GetComponent<Rigidbody> ().velocity = player.GetComponent<Rigidbody> ().velocity * 0.5f;
+					transitionToStateG (target);
+					targetHit = false;	
+				} else {
+					transitionToStateF (target);
+				}
 				break;
-			case State.G:
+		case State.G:
 				//If target hit
 				
-				transitionToStateA();
+			transitionToStateD ();
+			//endThrowing ();
 				break;
             default:
 
@@ -279,21 +327,25 @@ public class AIDemoController : MonoBehaviour
                 break;
         }
     }
-		
 
-	void throwAball(Vector3 direction)
+	void throwTheBall()
 	{
 		theBall.transform.SetParent(null);
 		theBall.GetComponent<Rigidbody> ().isKinematic = false;
 		theBall.GetComponent<Rigidbody> ().useGravity = true;
 		theBall.GetComponent<Collider> ().enabled = true;
-		theBall.GetComponent<Rigidbody>().AddForce(direction * throwingForce, ForceMode.Impulse);
-//		theBall.GetComponent<Rigidbody>().useGravity = true;
-//		theBall.GetComponent<Rigidbody>().AddForce(direction * throwingForce, ForceMode.Impulse);
+		theBall.GetComponent<Rigidbody>().AddForce(player.transform.forward * throwingForce, ForceMode.Impulse);
 		throwing = true;
 	}
 
 	void endThrowing(){
 		throwing = false;
+		Debug.Log ("Position should be changed!!!!");
+		theBall.transform.position = leftWrist.transform.position;
+		theBall.transform.SetParent (leftWrist.transform);
+		Debug.Log ("Balls's grand parent: " + theBall.transform.parent.name);
+		theBall.GetComponent<Rigidbody> ().isKinematic = true;
+		theBall.GetComponent<Rigidbody> ().useGravity = false;
+		theBall.GetComponent<Collider> ().enabled = false;
 	}
 }
